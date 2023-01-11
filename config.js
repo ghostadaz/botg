a58a281ad2dcbf5a7678fad54cb3e40061b415e6
ghostadaz <122482879+ghostadaz@users.noreply.github.com>
2023-01-11 22:26:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+     // replace the value below with the Telegram token you receive from @BotFather
+     var token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+     // firebase config from console.firebase
+     // Your web app's Firebase configuration
+     var config = {
+         apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+         authDomain: "XXXXXXXXXXX.firebaseapp.com",
+         databaseURL: "https://XXXXXXXXX.firebaseio.com/",
+         projectId: "XXXXXXXX",
+         storageBucket: "XXXXXXXXXXXX.appspot.com",
+         messagingSenderId: "XXXXXXXXXXXXX",
+         appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+         measurementId: "X-XXXXXXXXX",
+     };
+
+     exports.token = token;
+     exports.firebase_config = firebaseConfig;
